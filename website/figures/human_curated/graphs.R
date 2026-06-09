08d981539ae206a03193f3ba4d5462343c2394ed
koefoeden <koefoeden@gmail.com>
2026-06-09 14:59:11 +0200

    add genetic enrichment to new immune aggregation

--- website/figures/human_curated/graphs.R
+++ website/figures/human_curated/graphs.R
@@ -1,5 +1,26 @@
+# Main pipeline
+# Generates important processing intermediates, without being contamianted with downstream plots
+# Can subsequently be used to subset into 4 parts (parallel, GEX, ATAC and WNN)
+# using website/figures/human_curated/ATAC_keep_patterns.txt and similar for parallel, GEX and WNN
 targets::tar_glimpse(
-  names = tidyselect::matches("immune_human_2x"),
+  names = tidyselect::matches("multimodal_Seurat_object.immune_human_2x"),
 ) |>
   targets_graph_mermaid_lines() |>
-  writeLines("full.mmd")
+  writeLines("website/figures/human_curated/multimodal_Seurat_object.immune_human_2x.mmd")
+
+# Genetic enrichment
+targets::tar_glimpse(
+  names = tidyselect::matches("immune_human_2x") &
+    targets::tar_described_as(tidyselect::contains("checkpoint:genetic_enrichment"))
+) |>
+  targets_graph_mermaid_lines() |>
+  writeLines("website/figures/human_curated/checkpoint.genetic_enrichment.immune_human_2x.mmd")
+
+# Differential analyses
+# Needs
+targets::tar_glimpse(
+  names = tidyselect::matches("immune_human_2x") &
+    targets::tar_described_as(tidyselect::contains("checkpoint:differential_analyses"))
+) |>
+  targets_graph_mermaid_lines() |>
+  writeLines("website/figures/human_curated/checkpoint.differential_analyses.immune_human_2x.mmd")
